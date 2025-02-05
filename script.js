26dc691e83563d3a6eead1695c8d93b46751f4da
jonah <jonah.softwaredev@gmail.com>
2025-02-05 08:20:38 +0300

    feat: add event listeners and functions

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
 const container = document.querySelector(".container")
-let gridSize = 16
-let widthSize = container.clientWidth / gridSize
-let heightSize = container.clientHeight / gridSize
-let grid = []
-let isMousedown = false
+let inputValue = document.querySelector("input")
+let clearAll = document.querySelector("button")
 
-for (i = 0; i < gridSize * gridSize; i++) {
-    grid[i] = document.createElement("div")
-    grid[i].classList.add("cell")
-    
+function createGrid(gridSize) {
+    let grid = []
+    let widthSize = container.clientWidth / gridSize
+    let heightSize = container.clientHeight / gridSize
 
-    grid[i].style.flexGrow = "1"
-    grid[i].style.width = `${widthSize}px`
-    grid[i].style.height = `${heightSize}px`
-    container.appendChild(grid[i])
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        grid[i] = document.createElement("div")
+        grid[i].classList.add("cell")
+        
+
+        grid[i].style.flexGrow = "1"
+        grid[i].style.width = `${widthSize}px`
+        grid[i].style.height = `${heightSize}px`
+        container.appendChild(grid[i])
+    }
 
 }
 
-document.querySelectorAll(".cell").forEach(box => {
-    box.addEventListener("mousedown", () => {
-        isMousedown = true
-        box.style.backgroundColor = "black"
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function sketchPad() {
+    let isMousedown = false
+
+    document.querySelectorAll(".cell").forEach(box => {
+        box.addEventListener("mousedown", () => {
+            isMousedown = true
+            box.style.backgroundColor = "black"
+            console.log("here")
+        })
+
+        box.addEventListener("mouseover", () => {
+            if (isMousedown) {
+                box.style.backgroundColor = "black"
+            }
+        })
     })
 
-    box.addEventListener("mouseover", () => {
-        if (isMousedown) {
-            box.style.backgroundColor = "black"
-        }
+    document.addEventListener("mouseup", () => {
+        isMousedown = false
     })
+}
+
+addEventListener("DOMContentLoaded", () => {
+    createGrid(inputValue.value)
+    sketchPad()
 })
 
-document.addEventListener("mouseup", () => {
-    isMousedown = false
+
+inputValue.addEventListener("click", () => {
+    clearGrid()
+    gridSize = inputValue.value
+    sketchPad(createGrid(gridSize))
+})
+
+clearAll.addEventListener("click", () => {
+    document.querySelectorAll(".cell").forEach(box => {
+        box.style.backgroundColor = "white"
+    })
 })
